b905ce8e949711b3497f7bb53427cfc6aea82bf7
WebRachDotCom <haraka.rachid@gmail.com>
2022-08-27 00:50:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(expressLayouts)
 app.set("view engine", "ejs")
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: 'secret',
     resave: true, 
     saveUninitialized: true
 }))
