2ba21da650eae753334b8fa634fe8630619ba879
WebRachDotCom <haraka.rachid@gmail.com>
2022-08-27 00:46:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -262,7 +262,6 @@ app.post('/login', (req, res, next) => {
 
 // })
 
-app.listen(process.env.PORT || 3001, () => {
-    // var port = server.address().port;
-    console.log(`Server running on 3001`);
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
